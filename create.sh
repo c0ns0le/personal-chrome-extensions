a4cd5c579157c27c822fad65da2697cd5edfb15e
Razz Fox <razzintown@gmail.com>
2014-03-20 23:08:15 -0700

    A create script

---
+++ create.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function create_extension_folder {
+  mkdir "$1"
+  cd "$1"
+
+  if [[ $# == 3 ]]
+    then
+    CHRIMAGE="$3"
+  else
+    CHRIMAGE=128
+    touch "$CHRIMAGE.png"
+  fi
+
+  echo "{" > manifest.json
+  echo "\"manifest_version\": 2," >> manifest.json
+  echo "\"name\": \"$1\"," >> manifest.json
+  echo "\"description\": \"Link to $1\"," >> manifest.json
+  echo "\"version\": \"1.0\"," >> manifest.json
+  echo "\"app\": {" >> manifest.json
+  echo "    \"launch\": {" >> manifest.json
+  echo "        \"web_url\": \"$2\"" >> manifest.json
+  echo "       }" >> manifest.json
+  echo "   }," >> manifest.json
+  echo "" >> manifest.json
+  echo "\"icons\": {" >> manifest.json
+  echo "    \"128\": \"$CHRIMAGE.png\"" >> manifest.json
+  echo "   }" >> manifest.json
+  echo "}" >> manifest.json
+}
+
+create_extension_folder "$@"
